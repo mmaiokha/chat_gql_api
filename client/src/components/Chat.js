8b6b87e2b027369ddca095ed3a496047da6662d7
Glipher G <imnotsmarttt@gmail.com>
2023-08-14 12:38:12 +0300

    feat: project init

---
+++ client/src/components/Chat.js
@@ -0,0 +1,29 @@
+import {useAddMessage, useMessages} from '../lib/graphql/hooks';
+import MessageInput from './MessageInput';
+import MessageList from './MessageList';
+
+function Chat({user}) {
+    const {messages, loading, error} = useMessages();
+    const {addMessage} = useAddMessage();
+
+    const handleSend = async (text) => {
+        const message = await addMessage(text);
+    };
+
+    if(loading) {
+      return <div>Loading...</div>
+    }
+    return (
+        <section className="section">
+            <div className="container">
+                <h1 className="title is-4">
+                    {`Chatting as ${user}`}
+                </h1>
+                <MessageList user={user} messages={messages}/>
+                <MessageInput onSend={handleSend}/>
+            </div>
+        </section>
+    );
+}
+
+export default Chat;
---
+++ client/src/lib/graphql/queries.js
@@ -0,0 +1,32 @@
+import {gql} from '@apollo/client';
+
+export const messagesQuery = gql`
+    query MessagesQuery {
+        messages {
+            id
+            user
+            text
+        }
+    }
+`;
+
+export const addMessageMutation = gql`
+    mutation AddMessageMutation($text: String!) {
+        message: addMessage(text: $text) {
+            id
+            user
+            text
+        }
+    }
+`;
+
+export const messageAddedSubscription = gql`
+    subscription MessageAddedSubscription {
+        message: messageAdded {
+            id
+            text
+            user
+        }
+    }
+
+`
